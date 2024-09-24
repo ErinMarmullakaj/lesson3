b7eb2d82ede41f8d8b0a61cd6c4bc7cfe4e0f457
ErinMarmullakaj <erin.marmullakaj2@gmail.com>
2024-09-24 17:48:28 +0200

    ...

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+
+// $num = 20;
+
+// if(($num >= 12)&&($num <=19)){
+//     echo"you r a teenager";
+// }else if($num < 12){
+//     echo"you are a child";
+// }else{
+//     echo"you are an adult";
+// }
+
+// $a = 10;
+// $b = 15;
+// if($a == $b){
+//     echo "yes";
+// }else{
+//     echo"no";
+// }
+
+// $var = "";
+
+// switch($var){
+//     case 'red':
+//     echo "the color is red";
+//     break;
+
+//     case'blue':
+//         echo"the color is blue ";
+//         break;
+//         default:
+//         echo"there is no color";
+//         break;
+// }
+
+// $x = 1;
+
+// while(
+//     $x <= 10 
+// ){
+//     echo "the number is $x <br>";
+//     $x++;
+// }
+
+
+// $x = 11;
+
+// do{
+//     echo"the number is $x <br>";
+//     $x++;
+// }while(
+//     $x <= 10
+// );
+
+
+// for($i=0; $i < 10; $i++){
+//     echo "the number is $1 <br>";
+// }
+
+$car = array("BMV", "BENZ", "TESLA");
+
+foreach($car as $value){
+    echo"$value <br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
